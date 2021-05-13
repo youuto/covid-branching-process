d89e4d029279f551bfbbf304f91f9a8eae7fb42c
youuto <jskopek9@gmail.com>
2021-05-13 05:07:01 +0200

    Update model_import.R

--- model_import.R
+++ model_import.R
@@ -1,31 +1,34 @@
 import <- read.csv("import.csv", header = FALSE)
 import <- as.matrix(import)
 imp_tab <- as.data.frame(table(import))
-days <- as.integer(as.vector(imp_tab$Freq))        # nacitanie a spracovanie udajov
+days <- as.integer(as.vector(imp_tab$Freq))       
 
 set.seed(2345)
-n <- 100000                # pocet simulacii
-RI <- R0                   # parameter: zakladne reprodukcne cislo R0
-rI <- est_r                # disperzny parameter
-zI <- imp_tab$import       # pocet importovanych pripadov
-pZI <- days/sum(days)      # pravdepodobnosti poctu import. prip.
+n <- 100000               
+RI <- R0                  
+rI <- est_r                
+zI <- imp_tab$import      
+pZI <- days/sum(days)      
 for (i in 1:4)
 {
-  cat("generating generation",i,"\n")               #vypise kolku generaciu pocita
-  ZI <- replicate(n,sum(rnbinom(sample(zI,1,prob=pZI,replace=TRUE),mu=RI,size=rI)))
+  cat("generating generation",i,"\n")              
+  ZI <- replicate
+          (n, sum( rnbinom
+                   ( sample(zI, 1, prob = pZI,
+                                replace = TRUE),
+                                mu = RI,size = rI)))
   ZItab <- as.data.frame(table(ZI))
   zI <- as.integer(as.vector(ZItab$ZI))
-  pZI <- as.vector(ZItab$Freq/sum(ZItab$Freq))    # G-W proces
+  pZI <- as.vector(ZItab$Freq/sum(ZItab$Freq))    
 }
 FZI <- numeric(length(pZI))    
 FZI[1] <- pZI[1]
 for (i in 2:length(pZI))
 {
-  FZI[i] <- FZI[i-1]+pZI[i]  # distribucna funkcia poctu nakazencych 4-tej generacie
+  FZI[i] <- FZI[i-1]+pZI[i]  
 }
 ZItab <- cbind(ZItab,pZI,FZI) 
-ZItab                          # vysledna tabulka
-hist(ZI)                      # histogram prislusnej generacie ktoru sme vypocitali
+                      
 
 
 
